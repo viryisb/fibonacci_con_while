c2afd35181cd1cf049b411012b23006e67a15340
viryisb <44821968+viryisb@users.noreply.github.com>
2019-02-07 11:05:23 -0300

    con while

---
+++ fibo.js
@@ -0,0 +1,57 @@
+    /*var x = 1; 
+    var y = 1; 
+    var z; 
+    //var fin = 10
+    var fin = 32
+    
+    console.log("Partimos de: 1 conejo + 1 coneja= 2 conejos")
+ 
+    console.log(x) //este no es ciclo for
+    console.log(y) // este tampoco es ciclo for
+    z = x + y
+    console.log(z) //resultado de 1+1----> o sea 1 coneja + 1 conejo= 2 conejos 
+    console.log('--> a partir de ahora empieza el ciclo for:')
+    for (var i = 3; i < fin - 1; i++) { 
+     x = y
+      y = z 
+      z = y + x
+      console.log(z) 
+    }
+    
+    
+   var x = 1; 
+    var y = 1; 
+    var z; 
+    //var fin = 10
+    var fin = 32
+do {console.log(z) 
+  var i = 3; i < fin - 1; i++
+  x=x+y
+x=y
+y=z
+} while (z<fin); 
+
+var a=0, b = 1;
+while (b < 100) {
+ console.log (b),
+    a, b = b, a + b
+}*/
+
+var a=0;
+    var b=0;
+    var temp=1;
+    console.log (a+++temp)
+    do {    
+
+        a=b;
+        b=temp;
+        temp=a+b;
+        console.log(temp);
+      
+      
+    }while(temp<1000);
+   
+   
+     
+    
+   
